ef6e33f9e44c26fb930fd4aa800820449e416b57
ilikeshell <1007965673@qq.com>
2015-07-31 18:07:25 +0800

    加载一个扇区的文件能正确运行

--- boot/loader.asm
+++ boot/loader.asm
@@ -5,4 +5,13 @@ org	0100h
 	mov al, 'L'
 	mov [gs:((80 * 0 + 39) * 2)], ax
 	times 1024 nop
+	mov al, 'O'
+	mov [gs:((80 * 0 + 40) * 2)], ax
+	mov [gs:((80 * 0 + 41) * 2)], ax
+	mov al, 'D'
+	mov [gs:((80 * 0 + 42) * 2)], ax
+	mov al, 'E'
+	mov [gs:((80 * 0 + 43) * 2)], ax
+	mov al, 'R'
+	mov [gs:((80 * 0 + 44) * 2)], ax
 	jmp $
